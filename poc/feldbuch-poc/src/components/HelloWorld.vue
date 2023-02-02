7cfc6dea4d0d57edefd3c34180dd2203c5b81590
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2023-02-02 15:20:40 +0100

    init poc framework
    Created as a Vue.js App with Cordova for building Android, IOS and Browser versions

---
+++ poc/feldbuch-poc/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Created as a Vue.js App with Cordova</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'A first proof of concept for the archeological 3D-Fieldbook'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
