84008b0c3aed63b1ac5dbcf8ab2dfb12989149d6
Tobias Mink <xmrmagicx@gmail.com>
2023-04-23 11:07:47 +0200

    delete mesh animation
    Implented just for testing purposes

--- poc/feldbuch-poc/src/components/3DView.vue
+++ poc/feldbuch-poc/src/components/3DView.vue
@@ -55,8 +55,6 @@ export default {
     animate: function() {
 
         requestAnimationFrame(this.animate);
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
         this.renderer.render(this.scene, this.camera);
 
     }
