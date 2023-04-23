54280701f75e23309c94bccf8658a9792af9ce5f
Tobias Mink <xmrmagicx@gmail.com>
2023-04-23 11:06:19 +0200

    delete remaining GUI elements

--- poc/feldbuch-poc/src/components/3DView.vue
+++ poc/feldbuch-poc/src/components/3DView.vue
@@ -8,8 +8,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 export default {
+
   name: 'Viewer',
+
   data() {
+
     return {
       camera: null,
       scene: null,
@@ -18,9 +21,13 @@ export default {
       gui:null,
       mesh: null
     }
+
   },
+
   methods: {
+
     init: function() {
+
         let container = document.getElementById('container');
 
         this.camera = new THREE.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
@@ -42,23 +49,29 @@ export default {
 
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.update();
-
-        this.gui = new GUI(); //TODO: Match GUI placement with other elements on screen; currently behint topBar
-        this.gui.add( params, 'animate' ); 
         
     },
+
     animate: function() {
+
         requestAnimationFrame(this.animate);
         this.mesh.rotation.x += 0.01;
         this.mesh.rotation.y += 0.02;
         this.renderer.render(this.scene, this.camera);
+
     }
+
   },
+
   mounted() {
+
       this.init();
       this.animate();
+
   }
+
 }
+
 </script>
 
 <style scoped>
