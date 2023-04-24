030b7eb8cce530eca4c878e288b2a03f9efa71d7
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2023-04-24 16:33:24 +0200

    add remaining backend routes for models
    - GET all models
    - POST new model
    - PUT (update) existing model
    - DELETE existing model

--- poc/backend/routes/models.js
+++ poc/backend/routes/models.js
@@ -53,6 +53,41 @@ async function generateSignedUrl(fileName) {
 
 
 
+/* GET ALL models */
+router.get("/", async function (req, res, next) {
+  //get models from db
+  try {
+    var modelsArray = await models.get();
+  } catch (error) {
+    res.status(500).send("Error retrieving models from database");
+  }
+  
+  // generate signed URLs for every retrieved model
+  var modelsWithURLs = await Promise.all(modelsArray.docs.map( async (item) => {
+      try {
+        var modelURL = await generateSignedUrl(item.id + ".obj");
+      } catch (error) {
+        res.status(500).send("Error retrieving model URL: " + error);
+      }
+
+      try {
+        var textureURL = await generateSignedUrl(item.id + ".jpg");
+      } catch (error) {
+        res.status(500).send("Error retrieving texture URL: " + error);
+      }
+
+      const model = {
+        id: item.id,
+        project_id: item.data().project_id,
+        title: item.data().title,
+        model: modelURL,
+        texture: textureURL,
+      };
+      return model;
+  }));
+  res.status(200).send(modelsWithURLs);
+});
+
 /* GET model by ID */
 router.get("/:model_id", function (req, res, next) {
   models
@@ -76,4 +111,46 @@ router.get("/:model_id", function (req, res, next) {
       res.status(404).send("Model not found: " + err);
     });
 });
+
+/* POST new model */
+router.post("/", async function (req, res, next) {
+  models
+    .doc()
+    .set(req.body)
+    .then((response) => {
+      res.status(200).send("Added model");
+    })
+    .catch((err) => {
+      res.status(404).send("Couldn't add model: " + err);
+    });  
+});
+
+/* UPDATE model by ID*/
+router.put("/:model_id", function (req, res, next) {
+  console.log(req.body)
+  models
+    .doc(req.params.model_id)
+    .update(req.body)
+    .then((response) => {
+      res.status(200).send("Updated project: " + req.params.model_id);
+    })
+    .catch((err) => {
+      res.status(404).send("Couldn't update project: " + err);
+    });
+});
+
+/* DELETE model by ID*/
+router.delete("/:model_id", async function (req, res, next) {
+  models
+    .doc(req.params.model_id)
+    .delete({ exists: true })
+    .then((response) => {
+      //TODO: delete model and texture from firestore file-server
+      res.status(200).send("Deleted project: " + req.params.model_id);
+    })
+    .catch((err) => {
+      res.status(404).send("Couldn't delete project: " + err);
+    });
+});
+
 module.exports = router;
