7c58b4f4287e24bc61feb18f33336d76f13119bd
Tobias Mink <xmrmagicx@gmail.com>
2023-04-27 16:35:16 +0200

    change 3DView.vue ofline database component
    Component will be exported in external file in followup commit; File cleaned

--- poc/feldbuch-poc/src/components/3DView.vue
+++ poc/feldbuch-poc/src/components/3DView.vue
@@ -7,14 +7,6 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
-const DB_GEOMETRY = 'Geometry'
-const DB_GEOMETRY_VERSION = 1 /* must be incrementet with further adjustment */
-const STORE_EXCAVATION_GEO = 'excavation'
-
-const DB_IMAGE = 'Image'
-const DB_IMAGE_VERSION = 1 /* must be incrementet with further adjustment */
-const STORE_EXCAVATION_IMG = 'excavation'
-
 export default {
 
   name: 'Viewer',
@@ -28,19 +20,11 @@ export default {
       renderer: null,
       controls: null,
 
-      geometryDB: null,
-      imageDB: null,
-      geometries: [],
-      images: [],
-
     }
 
   },
 
   async mounted() {
-
-    await this.updateLocalDB()
-    await this.updateLocalStore()
 
     this.init();
     this.animate();
@@ -48,177 +32,6 @@ export default {
   },
 
   methods: {
-
-    /**
-     * IndexdDB:
-     *  updateLocalDB       - Update local database variables
-     *  updateLocalDB       - Update local object store variables
-     *  getDB               - Gets the current state of the database, or creates 
-     *                        a new database and objectstore
-     *  getObjects          - Gets all objects from object store
-     *  addObject           - Add object to object store 
-     *  deleteObject        - Delete object from object store
-     *  deleteAllObjects    - Delete object store from database
-     *  deleteDB            - Delete database
-     */
-
-     updateLocalDB: async function() {
-
-      this.geometryDB = await this.getDB( DB_GEOMETRY, DB_GEOMETRY_VERSION, 
-                                          STORE_EXCAVATION_GEO );
-      this.imageDB = await this.getDB( DB_IMAGE, DB_IMAGE_VERSION, 
-                                       STORE_EXCAVATION_IMG );
-
-    },
-
-    updateLocalStore: async function() {
-
-      this.geometries = await this.getObjects( this.geometryDB, 
-                                               STORE_EXCAVATION_GEO );
-      this.images = await this.getObjects( this.imageDB, 
-                                           STORE_EXCAVATION_IMG );
-
-    },
-
-    /**
-     * @param {String} dbName    - Database Name 
-     * @param {Int} dbVersion    - Database Version
-     * @param {String} storeName - Object store which is going compared, 
-     *                             created
-     */
-     getDB: async function( dbName, dbVersion, storeName ) {
-
-      return new Promise( ( resolve, reject ) => {
-
-        const request = window.indexedDB.open( dbName, dbVersion )
-
-        request.onerror = e => {
-          console.log( 'Error opening db', e );
-          reject( 'Error' );
-        }
-
-        request.onsuccess = e => {
-          resolve(e.target.result);
-        }
-
-        /* Gets current content of object store, or creates new one */
-        request.onupgradeneeded = e => {
-          console.log( 'onupgradeneeded' );
-          const db = e.target.result;
-          const objectStore = db.createObjectStore( storeName, 
-                                                    { autoIncrement: true, 
-                                                    keyPath:'id' } );
-        }
-
-      });
-
-    },
-
-    /**
-     * @param {any} whichDB   - Database which contains the object store
-     * @param {String} storeName - Object store, data is copied from
-     */
-     getObjects: async function( whichDB, storeName ) {
-      
-      return new Promise( ( resolve, reject ) => {
-
-        const trans = whichDB.transaction( [ storeName ], 'readonly' );
-        trans.oncomplete = e => {
-          resolve( data );
-        }
-
-        const store = trans.objectStore( storeName );
-        let data = [];
-
-        store.openCursor().onsuccess = e => {
-          const cursor = e.target.result;
-          if ( cursor ) {
-            data.push( cursor.value );
-            cursor.continue();
-          }
-        }
-
-      });
-    
-    },
-
-    /**
-     * @param {*} data      - Data to be added
-     * @param {*} whichDB   - Database which contains the object store
-     * @param {*} storeName - Object store, data is going to be added to
-     * @param {*} localArr  - Local array with data (gets updated)
-     */
-    addObject: async function(data, whichDB, storeName, localArr) {
-      
-      this.addDisabled = true;
-
-      /* --- DEBUGGING --- */
-      console.log( 'about to add ' + JSON.stringify( data ) );
-
-      await new Promise( ( resolve, reject ) => {
-
-        const trans = whichDB.transaction( storeName, 'readwrite' );
-        trans.oncomplete = e => {
-          resolve();
-        }
-
-        const store = trans.objectStore( storeName );
-        store.add( data );
-
-      })
-
-      localArr = await this.getObjects( whichDB, storeName );
-
-      this.addDisabled = false;
-
-    },
-
-    /**
-     * @param {*} id        - ID of Data to be deleted
-     * @param {*} whichDB   - Database which contains the object store
-     * @param {*} storeName - Object store, data is going to be deleted from
-     * @param {*} localArr  - Local array with data (gets updated)
-     */
-    deleteObject: async function(id, whichDB, storeName, localArr) {
-
-      /* --- DEBUGGING --- */
-      console.log( 'about to delete Object id: ' + id );
-
-      await new Promise( ( resolve, reject ) => {
-
-        const trans = whichDB.transaction( [ storeName ], 'readwrite' );
-        trans.oncomplete = e => {
-          resolve();
-        }
-
-        const store = trans.objectStore( storeName );
-        store.delete(id);
-
-      });
-      
-      localArr = await this.getObjects( whichDB, storeName );
-
-    },
-
-    /**
-     * @param {*} whichDB   - Database which contains the object store
-     * @param {*} storeName - Object store which is going to be deleted
-     * @param {*} localArr  - Local array with data (gets updated)
-     */
-    deleteAllObjects: async function(whichDB, storeName, localArr) {
-
-      const trans = whichDB.transaction( [ storeName ], 'readwrite' );
-      
-      const store = trans.objectStore( storeName );
-
-      const storeRequest = store.clear();
-      storeRequest.onsuccess = e => {
-        console.log( storeName + ' erased' );
-      }
-
-      localArr = await this.getObjects( whichDB, storeName );
-
-    },
 
     init: function() {
 
