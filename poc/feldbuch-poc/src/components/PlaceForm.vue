e1b0504836db36f6b95e53e025f1514cf887c1ea
Tobias Mink <xmrmagicx@gmail.com>
2023-05-29 11:30:00 +0200

    update functionalitys
    Overhaul

--- poc/feldbuch-poc/src/components/PlaceForm.vue
+++ poc/feldbuch-poc/src/components/PlaceForm.vue
@@ -1,6 +1,5 @@
 <template>
   <v-form ref="form">
-      <Navigation/>
     <v-tabs vertical color="secondary">
       <v-tab> Allgemein </v-tab>
       <v-tab> Positionen </v-tab>
@@ -8,75 +7,115 @@
       <v-tab-item class="px-4">
         <v-text-field v-model="place.ansprache" label="Ansprache"
           hint="Geben Sie hier eine Ansprache ein"></v-text-field>
-        <v-text-field v-model="place.date" label="Datierung" hint="Format: dd.mm.yyyy"></v-text-field>
+        <v-text-field v-model="place.date" label="Datierung" 
+                      hint="Format: dd.mm.yyyy"></v-text-field>
       </v-tab-item>
 
       <v-tab-item class="px-4">
-        <v-subheader v-if="positions <= 0"> Bisher wurden keine Positionen angegeben</v-subheader>
         <div id="wrapper">
           <v-form>
             <v-list>
               <v-divider></v-divider>
+              
+              <v-subheader v-if="positions.length === 0"> 
+                Bisher wurde keine Positionen angelegt
+              </v-subheader>
+              
               <template v-for="(position, i) in positions">
                 <v-list-item class="positionItem mt-3">
+                  
                   <v-list-item-content>
-                    <v-list-item-title class="text-h6"> Nr. {{ position.positionNumber }} - {{ position.date
-                    }}</v-list-item-title>
-                    <v-list-item-subtitle class="text-subtitle-1">{{ position.description }} </v-list-item-subtitle>
+                    
+                    <v-list-item-title class="text-h6">
+                      Nr. {{ position.positionNumber }} - {{ position.date }}
+                    </v-list-item-title>
+                    
+                    <v-list-item-subtitle class="text-subtitle-1">
+                      {{ position.description }}
+                    </v-list-item-subtitle>
+
                   </v-list-item-content>
-                  <v-btn color="primary"><v-icon color="white"
-                      v-on:click="modifyPosition(position.id)">mdi-pencil</v-icon></v-btn>
+                  
+                  <v-btn color="primary">
+                    <v-icon color="white"
+                      v-on:click="moveToPosition(position.id)">mdi-pencil
+                    </v-icon>
+                  </v-btn>
+
                 </v-list-item>
                 <v-divider v-if="i !== positions.length - 1"></v-divider>
               </template>
             </v-list>
-            <v-btn v-on:click="modifyPosition('new')" class="mr-16 mt-3" color="primary"> Position hinzufügen </v-btn>
+            <v-btn v-on:click="addPosition()" class="mr-16 mt-3" 
+                   color="primary"> Position hinzufügen </v-btn>
           </v-form>
         </div>
       </v-tab-item>
 
-      <v-btn v-on:click="savePlace()" color="secondary" class="py-6" tile> Speichern </v-btn>
+      <v-btn v-on:click="savePlace()" color="secondary" class="py-6" tile> 
+        Speichern
+      </v-btn>
+
       <v-dialog v-model="dialog" max-width="290">
         <template v-slot:activator="{ on, attrs }">
+
           <v-btn color="secondary" class="py-6" tile v-bind="attrs" v-on="on">
-            Delete
+            Löschen
           </v-btn>
+
         </template>
         <v-card>
+
           <v-card-title class="text-h5">
-            Do you really want to delete the place:  <br> "{{ place.id }}"?
+            Wollen Sie die Stelle "{{ place.placeNumber }}" wirklich löschen?
           </v-card-title>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="secondary" v-on:click="deletePlace()" @click="dialog = false">
-              Yes
+
+            <v-btn color="secondary" v-on:click="deletePlace()"
+                   @click="dialog = false">
+              Ja
             </v-btn>
+
             <v-btn color="primary" @click="dialog = false">
-              No
+              Nein
             </v-btn>
+
           </v-card-actions>
         </v-card>
       </v-dialog>
-      <v-btn v-on:click="goBack" color="primary" class="py-6" tile> Cancel</v-btn>
+
+      <v-btn v-on:click="goBack" color="primary" class="py-6" tile> 
+        Abbrechen
+      </v-btn>
 
     </v-tabs>
+
     <v-alert v-model="error_dialog" type="error" dense outlined dismissible>
       {{ error_message }}
     </v-alert>
+
   </v-form>
 </template>
 
 <script>
+/**
+ * Methods overview:
+ *  updatePlace     - Updates existing presentation of the place
+ *  updatePositions - Updates existing presentation of positions
+ *  moveToPosition  - Loads the view of the selected position
+ *  addPosition     - Adds a new position to the list
+ *  savePlace       - Saves the current processing status
+ *  deletePlace     - Deletes the currently selected place
+ */
 import VueCookies from 'vue-cookies';
 import { fromOfflineDB } from '../ConnectionToOfflineDB.js'
-import Navigation from './Navigation.vue'
 
 export default {
+
   name: 'PlaceCreation',
-  components:{
-    Navigation
-  },
-  data() {
+
+  data() {                                                                      /* -------> data() */
 
     return {
       place: null,
@@ -85,60 +124,110 @@ export default {
       error_message: '',
       is_new: true,
       is_required: [v => !!v || 'Pflichtfeld'],
-      dialog: false
+      dialog: false,
     }
 
   },
-  async created() {
 
-    await fromOfflineDB.syncLocalDBs();
-    await this.updateData();
+  async created() {                                                             /* -------> created() */
+
+    await fromOfflineDB.syncLocalDBs();                                         
+    await this.updatePlace();
+    await this.updatePositions();
 
   },
-  methods: {
-    /* Update existing data */
-    async updateData() {
 
-      /* Get id of selected place */
-      const currentPlace = VueCookies.get("currentPlace")
+  methods: {                                                                    /* -------> methods */
 
-      /* Get all data of selected place */
-      const data = await fromOfflineDB.getObject(currentPlace, 'Places', 'places');
-      this.place = data.result
+    async updatePlace() {                                                       /* ------>> updatePlace() */
 
-      /* Get all positions of selected place */
-      this.positions = await fromOfflineDB.getAllObjects('Positions', 'positions');
+      const currentPlace = VueCookies.get( 'currentPlace' );                    /* Get id of selected place */
+      const data = await fromOfflineDB.getObject                                /* Get all data of selected place */
+                         ( currentPlace, 'Places', 'places' );
+      this.place = data.result;                                                 /* Update 'place' with existing data */
 
-      this.positions = this.positions.filter(pos => pos.place_id === currentPlace)
     },
-    modifyPosition: function(item_id){
-      if (item_id !== 'new') {
-        VueCookies.set('currentPosition', item_id)
+
+    async updatePositions() {                                                   /* ------>> updatePositions() */
+      
+      this.positions = await fromOfflineDB.getPositionsWithPlaceID              /* Update 'positions' with existing data */
+                             ( this.place.id, 'Positions', 'positions' );
+
+    },
+
+    moveToPosition( positionID ) {                                              /* ------>> moveToPosition() */
+      
+      if( positionID !== 'new' ) {
+        VueCookies.set( 'currentPosition', positionID );
+      };
+
+      this.$router.push( { name: 'PositionCreation'} );
+
+    },
+
+    async savePlace() {
+
+      await fromOfflineDB.deleteObject( this.place.id, 'Places', 'places' );
+      await fromOfflineDB.addObject( this.place, 'Places', 'places' );
+
+    },
+
+    async deletePlace() {                                                       /* ------>> deletePlace() */
+
+      await fromOfflineDB.deleteObject(this.place.id, 'Places', 'places');      /* Delete the selected place ... */
+      VueCookies.remove('currentPlace');                                        /* ... and remove cookie */
+      
+      this.$router.push({ name: "PlacesOverview" });                            /* Return to PlacesOverview page */
+
+      await fromOfflineDB.deleteCascadePositions                                /* Delete all positions wich were connected with the selected place ... */
+            (this.place.id, 'Positions', 'positions');
+      VueCookies.remove('currentPosition');                                     /* ... and remove cookies */
+
+    },
+
+    async addPosition() {                                                       /* ------>> addPositions() */
+
+      const newPosition = {
+        id:             String(Date.now()),
+        positionNumber: 0,
+        placeID:        this.place.id,
+        texts: {
+          firstText: "Imagine ... words ... wooooow! *mind blown*"
+        },
+        images: {
+          firstImage: "The thing with pixels"
+        },
+        models: {
+          firstModel: "Perfect shape!"
+        }
+      };
+
+      if(this.positions.length == 0) {
+        newPosition.positionNumber = 1
+      } else {
+        this.updatePositions();
+        const positionNumber = Math.max(...this.positions.map                   /* Get the highest positionNumber of existing positions */
+                                                (o => o.positionNumber))
+        const newPositionNumber = positionNumber + 1;                           
+        newPosition.positionNumber = newPositionNumber;                         /* Update the current positionNumber */
       }
-      this.$router.push({ name: 'PositionCreation', params: { position_id: item_id } })
-    },
-    savePlace: async function() {
 
-      await fromOfflineDB.deleteObject(this.place.id, 'Places', 'places')
-      await fromOfflineDB.addObject(this.place, 'Places', 'places')
+      await fromOfflineDB.addObject(newPosition, "Positions", "positions");     /* Add a new position to the store */
+      await this.updatePositions(newPosition.id);                               /* Update positions list */
 
     },
-    /* Delete the selected place */
-    deletePlace: async function () {
 
-      await fromOfflineDB.deleteObject(this.place.id, 'Places', 'places')
-      this.$router.push({ name: "PlacesOverview" });
-
-    },
-    /* Go back to PlacesOverview */
-    goBack: function () {
+    goBack() {                                                                  /* ------>> goBack() */
 
       this.$emit("view", "PlacesOverview");
       this.$router.push({ name: "PlacesOverview" });
 
     }
+
   }
+
 };
+
 </script>
 
 <style scoped></style>
