113764865043116aab9bc4f7d00c4ada0f09cd3c
Meoli <mertens.oliver@gmx.de>
2023-05-30 18:14:28 +0200

    update frontend activity list to vuetify 3

--- poc/fieldbook-client/src/views/ActivitiesOverview.vue
+++ poc/fieldbook-client/src/views/ActivitiesOverview.vue
@@ -8,8 +8,8 @@
           <v-chip>
             Derzeit ausgewählt
           </v-chip>
-            <v-list-item-title> {{ current_activity.id }} </v-list-item-title>
-            <v-list-item-subtitle> {{ current_activity.description }} </v-list-item-subtitle>
+          <v-list-item-title> {{ current_activity.id }} </v-list-item-title>
+          <v-list-item-subtitle> {{ current_activity.description }} </v-list-item-subtitle>
           <v-chip variant="elevated">
             Derzeit ausgewählt
           </v-chip>
@@ -17,55 +17,73 @@
         <v-divider></v-divider>
         <v-list-subheader v-if="activities.length === 0"> Bisher wurden keine Aktivitäten angelegt</v-list-subheader>
         <template v-for="(activity, i) in activities" :key="activity">
-            <div v-if="!activity.edit">
-              <v-row>
-                <v-col>
-                  <v-list-item v-on:click="setActivity(activity.id)">
-                    <v-list-item-title class="ma-4"> {{ activity.activityNumber }} </v-list-item-title>
-                  </v-list-item>
-                </v-col>
-
-                <v-col>
-                  <v-btn class="ma-1" icon color="purple" v-on:click="modifyActivity(activity)">
-                    <v-icon>mdi-pencil</v-icon>
-                  </v-btn>
-
-                  <v-btn class="ma-1" icon color="red" v-on:click="deleteActivity(activity)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </div>
-            <div v-if="activity.edit">
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="3">
-                    <v-text-field :rules="[rules.required]" v-model="activity.außenstelle" label="Außenstelle"
-                      hide-details="auto" placeholder="Xanten" clearable></v-text-field>
+          <div v-if="!activity.edit">
+            <v-row>
+              <v-col cols="12">
+                <v-row no-gutters>
+                  <v-col cols="8">
+                    <v-sheet class="pa-2 ma-2">
+                      <v-list-item v-on:click="setActivity(activity.id)">
+                        <v-list-item-title class="ma-4"> {{ activity.activityNumber }} </v-list-item-title>
+                      </v-list-item>
+                    </v-sheet>
                   </v-col>
-
-                  <v-col cols="12" sm="6" md="3">
-                    <v-text-field :rules="[rules.required]" v-model="activity.jahr" label="Jahr" hide-details="auto"
-                      placeholder="2023" clearable counter maxlength="4"></v-text-field>
-                  </v-col>
-
-                  <v-col cols="12" sm="6" md="3">
-                    <v-text-field :rules="[rules.required, rules.counter]" v-model="activity.nummer" label="Nummer"
-                      hide-details="auto" placeholder="1337" clearable counter maxlength="4">
-                    </v-text-field>
-                  </v-col>
-
-                  <v-col cols="12" sm="6" md="3">
-                    <v-btn class="ma-1" icon color="purple" v-on:click="saveActivity(activity)">
-                      <v-icon>mdi-content-save-all</v-icon>
+                  <v-col cols="2" class="pa-4">
+                    <v-btn class="ma-1" icon color="purple" v-on:click="modifyActivity(activity)">
+                      <v-icon>mdi-pencil</v-icon>
                     </v-btn>
-                    <v-btn class="ma-1" icon color="red" v-on:click="clearActivityEditMask(activity)">
-                      <v-icon>mdi-close-circle</v-icon>
+
+                    <v-btn class="ma-1" icon color="red" v-on:click="deleteActivity(activity)">
+                      <v-icon>mdi-delete</v-icon>
                     </v-btn>
+
                   </v-col>
                 </v-row>
-              </v-container>
-            </div>
+              </v-col>
+            </v-row>
+          </div>
+          <div v-if="activity.edit">
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-row no-gutters>
+                    <v-col cols="3">
+                      <v-sheet class="pa-2 ma-2">
+                        <v-text-field :rules="[rules.required]" v-model="activity.außenstelle" label="Außenstelle"
+                          hide-details="auto" placeholder="Xanten" clearable></v-text-field>
+                      </v-sheet>
+                    </v-col>
+
+                    <v-col cols="3">
+                      <v-sheet class="pa-2 ma-2">
+                        <v-text-field :rules="[rules.required]" v-model="activity.jahr" label="Jahr" hide-details="auto"
+                          placeholder="2023" clearable counter maxlength="4"></v-text-field>
+                      </v-sheet>
+                    </v-col>
+
+                    <v-col cols="3">
+                      <v-sheet class="pa-2 ma-2">
+                        <v-text-field :rules="[rules.required, rules.counter]" v-model="activity.nummer" label="Nummer"
+                          hide-details="auto" placeholder="1337" clearable counter maxlength="4">
+                        </v-text-field>
+                      </v-sheet>
+                    </v-col>
+
+                    <v-col cols="2">
+                      <v-sheet class="pa-1 ma-2">
+                        <v-btn class="ma-1" icon color="purple" v-on:click="saveActivity(activity)">
+                          <v-icon>mdi-content-save-all</v-icon>
+                        </v-btn>
+                        <v-btn class="ma-1" icon color="red" v-on:click="clearActivityEditMask(activity)">
+                          <v-icon>mdi-close-circle</v-icon>
+                        </v-btn>
+                      </v-sheet>
+                    </v-col>
+                  </v-row>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
           <v-divider v-if="i !== activities.length - 1"></v-divider>
 
         </template>
@@ -74,43 +92,43 @@
       <v-container v-if="showInputMask">
 
         <v-row>
-      <v-col cols="12">
-        <v-row no-gutters>
-          <v-col cols="3">
-            <v-sheet class="pa-2 ma-2">
-              <v-text-field :rules="[rules.required]" v-model="activity.außenstelle" label="Außenstelle" hide-details="auto"
-              placeholder="Xanten" clearable></v-text-field>
-            </v-sheet>
-          </v-col>
+          <v-col cols="12">
+            <v-row no-gutters>
+              <v-col cols="3">
+                <v-sheet class="pa-2 ma-2">
+                  <v-text-field :rules="[rules.required]" v-model="activity.außenstelle" label="Außenstelle"
+                    hide-details="auto" placeholder="Xanten" clearable></v-text-field>
+                </v-sheet>
+              </v-col>
 
-          <v-col cols="3">
-            <v-sheet class="pa-2 ma-2">
-              <v-text-field :rules="[rules.required]" v-model="activity.jahr" label="Jahr" hide-details="auto"
-              placeholder="2023" clearable counter maxlength="4"></v-text-field>
-            </v-sheet>
-          </v-col>
+              <v-col cols="3">
+                <v-sheet class="pa-2 ma-2">
+                  <v-text-field :rules="[rules.required]" v-model="activity.jahr" label="Jahr" hide-details="auto"
+                    placeholder="2023" clearable counter maxlength="4"></v-text-field>
+                </v-sheet>
+              </v-col>
 
-          <v-col cols="3">
-            <v-sheet class="pa-2 ma-2">
-              <v-text-field :rules="[rules.required, rules.counter]" v-model="activity.nummer" label="Nummer"
-              hide-details="auto" placeholder="1337" clearable counter maxlength="4">
-            </v-text-field>
-            </v-sheet>
-          </v-col>
+              <v-col cols="3">
+                <v-sheet class="pa-2 ma-2">
+                  <v-text-field :rules="[rules.required, rules.counter]" v-model="activity.nummer" label="Nummer"
+                    hide-details="auto" placeholder="1337" clearable counter maxlength="4">
+                  </v-text-field>
+                </v-sheet>
+              </v-col>
 
-          <v-col cols="2">
-            <v-sheet class="pa-1 ma-2">
-              <v-btn class="ma-1" icon color="purple" v-on:click="saveActivity(activity)">
-              <v-icon>mdi-content-save-all</v-icon>
-            </v-btn>
-            <v-btn class="ma-1" icon color="red" v-on:click="clearActivityMask()">
-              <v-icon>mdi-close-circle</v-icon>
-            </v-btn>
-            </v-sheet>
+              <v-col cols="2">
+                <v-sheet class="pa-1 ma-2">
+                  <v-btn class="ma-1" icon color="purple" v-on:click="saveActivity(activity)">
+                    <v-icon>mdi-content-save-all</v-icon>
+                  </v-btn>
+                  <v-btn class="ma-1" icon color="red" v-on:click="clearActivityMask()">
+                    <v-icon>mdi-close-circle</v-icon>
+                  </v-btn>
+                </v-sheet>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
-      </v-col>
-    </v-row>
       </v-container>
       <v-btn v-on:click="modifyActivity('new')" color="primary" v-if="!showInputMask"> Aktivität hinzufügen </v-btn>
     </v-form>
@@ -134,7 +152,7 @@ export default {
       this.activities = await fromOfflineDB.getAllObjects('Activities', 'activities')
     },
     async setActivity(activity_id) {
-      if(VueCookies.get('currentActivity') !== activity_id) {
+      if (VueCookies.get('currentActivity') !== activity_id) {
         VueCookies.remove('currentPlace');
         VueCookies.remove('currentPosition');
       }
@@ -170,8 +188,8 @@ export default {
       VueCookies.remove('currentPlace');
       VueCookies.remove('currentActivity');
       this.$emit('view', 'Stelle')
-      await fromOfflineDB.deleteCascade(activity.id, 'place', 'Places', 
-                                        'places');
+      await fromOfflineDB.deleteCascade(activity.id, 'place', 'Places',
+        'places');
       await fromOfflineDB.deleteObject(activity.id, 'Activities', 'activities')
       await this.getActivities();
     },
@@ -228,6 +246,5 @@ export default {
 }
 </script>
   
-<style scoped>
-</style>
+<style scoped></style>
   
