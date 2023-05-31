cd5cf315350a5771e2bc230602ddca24c501a335
Meoli <mertens.oliver@gmx.de>
2023-05-31 17:10:06 +0200

    update frontend navigation
    now only the little slider bar under the buttontext is in color

--- poc/fieldbook-client/src/components/Navigation.vue
+++ poc/fieldbook-client/src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-tabs flat bg-color="accent" color="primary" fixed-tabs align-tabs="center">
-    <v-tab max-width="250px" class="text-h6" @click="changePage('ActivitiesOverview')" > AKTIVITÄTEN</v-tab>
+  <v-tabs flat bg-color="accent" fixed-tabs align-tabs="center" slider-color="primary">
+    <v-tab max-width="250px" class="text-h6" @click="changePage('ActivitiesOverview')">AKTIVITÄTEN</v-tab>
     <v-tab max-width="250px" class="text-h6" @click="changePage('PlacesOverview')" :disabled="!activityIsSet"> STELLEN</v-tab>
     <v-tab max-width="250px" class="text-h6" @click="changePage('PositionCreation')" :disabled="!placeIsSet"> POSITIONEN</v-tab>
   </v-tabs>
