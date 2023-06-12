ae1db8b8303ed7092ea6848c9be4846f7a842aab
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2023-06-12 17:57:04 +0200

    add VToast component

---
+++ poc/fieldbook-client/src/components/VToast.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-snackbar 
+        :color="color" 
+        :timeout="timer" 
+        v-model="showSnackbar" 
+        location="bottom">
+        <v-icon start>{{ icon }}</v-icon>
+        {{ message }}
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    name: "VToast",
+    data() {
+        return {
+            showSnackbar: false,
+            message: '',
+            color: 'success',
+            icon: 'mdi-check',
+            timer: 3000
+        }
+    },
+    methods: {
+        show(data) {
+            this.message = data.message || 'missing "message".'
+            this.color = data.color || 'success'
+            this.timer = data.timer || 3000
+            this.icon = data.icon || 'mdi-check'
+            this.showSnackbar = true
+        }
+    }
+}
+</script>
