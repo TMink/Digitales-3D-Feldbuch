5ad11312907f8f074551f234bcfaabb8085cde92
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2023-06-23 17:03:20 +0200

    remove global text-centering

--- poc/fieldbook-client/src/App.vue
+++ poc/fieldbook-client/src/App.vue
@@ -150,7 +150,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: var(--ion-color-light-contrast);
 }
 
