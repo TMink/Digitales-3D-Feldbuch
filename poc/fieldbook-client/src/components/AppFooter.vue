54d8d16e8cb8eab2a057e4f3cecbb5cd6eb31528
Meoli <mertens.oliver@gmx.de>
2023-07-09 16:42:53 +0200

    add th koeln icon as button to footer
    Button opens a new tab with the th koeln homepage of our faculty

--- poc/fieldbook-client/src/components/AppFooter.vue
+++ poc/fieldbook-client/src/components/AppFooter.vue
@@ -1,17 +1,21 @@
 <template>
     <v-footer app>
-        <v-col class="text-center">
-            {{ new Date().getFullYear() }} — <strong>TH-Köln</strong>
-        </v-col>
+        <v-row>
+            <v-col icon class="text-center">
+                {{ new Date().getFullYear() }} — <strong>TH-Köln</strong>
+                <v-btn icon class="ma-1" variant="text" href="https://www.th-koeln.de/informatik-und-ingenieurwissenschaften/fakultaet-fuer-informatik-und-ingenieurwissenschaften_2466.php" target="_blank">
+                    <v-img src="/THKöln_Icon.png"></v-img>
+                </v-btn>
+            </v-col>
+        </v-row>
     </v-footer>
 </template>
   
 <script>
-    export default {
-        name: 'AppFooter'
-    }
+export default {
+    name: 'AppFooter'
+}
 </script>
   
-<style>
-</style>
+<style></style>
   
