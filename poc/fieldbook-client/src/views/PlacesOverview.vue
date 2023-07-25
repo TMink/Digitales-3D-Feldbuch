29b58d9ffbb50fbbf29f0351b4533c006dae677b
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2023-07-25 18:32:59 +0200

    rework PlaceOverview; add 'show all info'-btn
    - 'show all info'-button extends the list view of all places to show all columns
    - fix problems with search filter
    - fix visual inconsistencies
    - fix variable screen sizes

--- poc/fieldbook-client/src/views/PlacesOverview.vue
+++ poc/fieldbook-client/src/views/PlacesOverview.vue
@@ -1,93 +1,267 @@
 <template>
   <div id="wrapper">
     <Navigation active_tab_prop="1" />
-    <v-row class="pt-4">
+    <v-row no-gutters class="align-center pa-2">
       <v-spacer></v-spacer>
-      <v-form class="w-75 pa-2">
+      <v-card class="ma-2 w-50" variant="text">
+        <v-row no-gutters class="align-center pa-2">
 
-        <!-- LIST TITLE -->
-        <v-card-text v-if="places.length !== 0">
-          <div>
-            <h3><v-row class="justify-center">
-                <v-col cols="1" class="text-left">
-                  Nr.
-                </v-col>
-                <v-col cols="6" class="text-left">
-                  Titel
-                </v-col>
-                <v-col cols="2" class="text-left">
-                  Datum
-                </v-col>
-              </v-row></h3>
-          </div>
-        </v-card-text>
-
-        <!-- PLACES LIST -->
-        <v-card>
-            <v-list-subheader v-if="places.length === 0">
-              {{ $t('not_created_yet', { object: $tc('place', 1) }) }}
-            </v-list-subheader>
-
-            <v-card-title>
-              <v-text-field
-                v-model="searchQuery"
-                append-icon="mdi-magnify"
-                label="Search"
-                single-line
-                hide-details
-              ></v-text-field>
-            </v-card-title>
-            <v-data-table-virtual
-              :items="filteredPlaces"
-              class="elevation-1"
-              :height="getTableHeight">
-              <template v-slot:item="{ item }">
-                <v-list-item v-on:click="moveToPlace(item.raw.id)">
-                    <v-row class="justify-center align-center my-2">
-                      <v-col cols="1">
-                        <v-list-item-title>
-                          {{ item.raw.placeNumber }}
-                        </v-list-item-title>
-                      </v-col>
-
-                      <v-col cols="6">
-                        <v-list-item-title 
-                          class="text-wrap" 
-                          v-if="item.raw.title.length != 0">
-                          {{ item.raw.title.join("; ") }}
-                        </v-list-item-title>
-
-                        <v-list-item-title 
-                          class="text-grey-darken-1" 
-                          v-if="item.raw.title.length == 0">
-                          {{ $t('title') }}
-                        </v-list-item-title>
-                      </v-col>
-
-                      <v-col cols="2">
-                        <v-list-item-title  
-                          v-if="item.raw.date.length != 0">
-                          {{ item.raw.date }}
-                        </v-list-item-title>
-
-                        <v-list-item-title 
-                          class="text-grey-darken-1" 
-                          v-if="item.raw.date.length == 0">
-                          {{ $t('date') }}
-                        </v-list-item-title>
-                      </v-col>
-
-                    </v-row>
-                    <v-divider></v-divider>
-                  </v-list-item>
-                </template>
-            </v-data-table-virtual>
-        </v-card>
-
-      </v-form>
+          <!-- SHOW ALL SWITCH-->
+          <v-col cols="3">
+            <v-switch
+              class="pl-3" 
+              v-model="showAllInfo"
+              hide-details 
+              label="Show all info">
+            </v-switch>
+          </v-col>
+          
+          <v-divider vertical class="ml-2 mr-6" />
+          <!-- PLACES SEARCH -->
+          <v-col cols="8">
+            <v-text-field 
+              v-model="searchQuery" 
+              append-icon="mdi-magnify" 
+              label="Search" 
+              single-line 
+              hide-details>
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-card>
       <v-spacer></v-spacer>
     </v-row>
 
+    <v-form class="px-6">
+      <v-row>
+        <v-spacer></v-spacer>
+        
+        <!-- PLACES TABLE SMALL -->
+        <v-card 
+          class="pa-3"
+          :min-width="windowWidth * 0.75">
+          <v-data-table-virtual 
+            v-show="!showAllInfo"
+            :items="filteredPlaces"
+            fixed-header
+            :height="getTableHeight"
+            :headers="headers">
+
+            <template v-slot:item="{ item, index }">
+              <tr 
+                v-on:click="moveToPlace(item.raw.id)"
+                @mouseenter="setHoveredRow(index, true)"
+                @mouseleave="setHoveredRow(index, false)">
+                <!-- PLACE NUMBER -->
+                <td class="py-6" :style="getRowStyle(index)">
+                  <v-list-item-title>
+                    {{ item.raw.placeNumber }}
+                  </v-list-item-title>
+                </td>
+                <!-- TITLE -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title 
+                    class="text-wrap">
+                    {{ item.raw.title.join("; ") || '-' }}
+                  </v-list-item-title>
+                </td>
+                <!-- DATE -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title>
+                    {{ item.raw.date || '-' }}
+                  </v-list-item-title>
+                </td>
+              </tr>
+            </template>
+          </v-data-table-virtual>
+        
+
+          <!-- PLACES LIST COMPLETE -->
+          <v-data-table-virtual 
+            fixed-header
+            v-show="showAllInfo"
+            :items="filteredPlaces" 
+            :height="getTableHeight"
+            :headers="fullHeaders">
+
+            <template v-slot:item="{ item, index }">
+              <tr v-on:click="moveToPlace(item.raw.id)" 
+                @mouseenter="setHoveredRow(index, true)"
+                @mouseleave="setHoveredRow(index, false)">
+
+                <!-- PLACE NUMBER -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title>
+                    {{ item.raw.placeNumber }}
+                  </v-list-item-title>
+                </td>
+
+                <!-- TITLE -->
+                <td class="py-2" :style="getRowStyle(index)">
+                  <v-list-item-title
+                    v-if="item.raw.title.length > 0"
+                    style="min-width:200px" 
+                    class="text-wrap">
+                    {{ item.raw.title.join("; ")}}
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                    v-if="item.raw.title.length == 0" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                </td>
+
+                <!-- NO FINDING -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title 
+                    v-if="item.raw.noFinding">
+                    &cross;
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                    v-if="!item.raw.noFinding" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                </td>
+                
+                <!-- REST FINDING -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title 
+                    v-if="item.raw.restFinding">
+                    &cross;
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                    v-if="!item.raw.restFinding" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                </td>
+
+                <!-- RIGHT VALUES -->
+                <td :style="getRowStyle(index)" class="align-right">
+                  <v-list-item-title 
+                    v-if="item.raw.right">
+                    {{ item.raw.right}}
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                    v-if="item.raw.rightTo">
+                    <span style="color:dimgrey">
+                      to
+                    </span>
+                    {{ item.raw.rightTo}}
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                    v-if="!item.raw.right" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                  <v-list-item-title 
+                    v-if="!item.raw.rightTo" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                </td>
+
+                <!-- UP VALUES -->
+                <td :style="getRowStyle(index)" class="align-right">
+                  <div v-if="item.raw.up">
+                    <v-list-item-title>
+                      {{ item.raw.up}}
+                    </v-list-item-title>
+                    <v-list-item-title>
+                      <span style="color:dimgrey; text-align: left;">
+                        to
+                      </span>
+                      {{ item.raw.upTo || '-' }}
+                    </v-list-item-title>
+                  </div>
+
+                  <div 
+                    v-if="!item.raw.upTo"
+                    style="color:dimgrey;">
+                    <v-list-item-title>
+                    -
+                    </v-list-item-title>
+                    <v-list-item-title>
+                      -
+                    </v-list-item-title>
+                  </div>
+                </td>
+
+                <!-- DEPTH -->
+                <td :style="getRowStyle(index)" class="align-right">
+                  <v-list-item-title 
+                    v-if="item.raw.depthTop">
+                    {{ item.raw.depthTop }}
+                    <span style="color:dimgrey">
+                      &#x25B2;
+                    </span>
+                  </v-list-item-title>
+                  <v-list-item-title 
+                    v-if="item.raw.depthBot">
+                    {{ item.raw.depthBot }}
+                    <span style="color:dimgrey">
+                      &#x25BC;
+                    </span>
+                  </v-list-item-title>
+
+                  <v-list-item-title 
+                  v-if="!item.raw.depthTop" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                  <v-list-item-title 
+                  v-if="!item.raw.depthBot" style="color:dimgrey;">
+                    -
+                  </v-list-item-title>
+                </td>
+
+                <!-- PLANE -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title class="text-wrap">
+                    {{ item.raw.plane || '-' }}
+                  </v-list-item-title>
+                </td>
+
+                <!-- VISIBILITY -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title 
+                    v-if="item.raw.visibility != ''">
+                    {{ $tc('visibilities', item.raw.visibility) }}
+                  </v-list-item-title>
+                  <v-list-item-title 
+                    v-if="item.raw.visibility == ''" style="color:dimgrey;">
+                      -
+                    </v-list-item-title>
+                </td>
+
+                <!-- DESCRIPTION -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title class="text-wrap">
+                    {{ item.raw.description || '-' }}
+                  </v-list-item-title>
+                </td>
+
+                <!-- EDITOR -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title>
+                    {{ item.raw.editor || '-' }}
+                  </v-list-item-title>
+                </td>
+
+                <!-- DATE -->
+                <td :style="getRowStyle(index)">
+                  <v-list-item-title>
+                    {{ item.raw.date || '-' }}
+                  </v-list-item-title>
+                </td>
+                <v-divider></v-divider>
+              </tr>
+            </template>
+          </v-data-table-virtual>
+        </v-card>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-form>
     <AddButton v-on:click="addPlace()" />
   </div>
 </template>
@@ -126,15 +300,38 @@ export default {
     return {
       places: [],
       searchQuery: '',
+      showAllInfo: false,
+      hoveredRow: -1,
       headers: [
         {
-          title: 'placeNumber',
+          title: 'Nr.',
           align: 'start',
           sortable: true,
           key: 'placeNumber',
+          width: "50px"
         },
-        { title: 'title', align: 'start', key: 'title' },
-        { title: 'date', align: 'end', key: 'date' },
+        { title: 'Title', align: 'start', key: 'title' },
+        { title: 'Date', align: 'start', key: 'date', width: "100px" },
+      ],
+      fullHeaders: [
+        {
+          title: 'Nr.',
+          align: 'start',
+          sortable: true,
+          key: 'placeNumber',
+          width: "25px"
+        },
+        { title: 'Title', align: 'start', key: 'title', width: "250px" },
+        { title: 'No\nFind', align: 'start', key: 'noFinding', width: "50px" },
+        { title: 'Rest\nFind', align: 'start', key: 'restFinding', width: "50px" },
+        { title: 'Right', align: 'end', key: 'right', width: "50px" },
+        { title: 'Up', align: 'end', key: 'up', width: "50px" },
+        { title: 'Depth', align: 'end', key: 'depthBot', width: "50px" },
+        { title: 'Plane', align: 'start', key: 'plane', width: "150px" },
+        { title: 'Visibility', align: 'start', key: 'visibility', width: "50px" },
+        { title: 'Description', align: 'start', key: 'description', width: "150px" },
+        { title: 'Editor', align: 'start', key: 'editor', width: "50px" },
+        { title: 'Date', align: 'start', key: 'date', width: "100px" },
       ],
     };
   },
@@ -296,8 +493,45 @@ export default {
       return (
         item.placeNumber.toString().match(re) ||
         item.title.join('; ').match(re) ||
+        item.right.toString().match(re) ||
+        item.rightTo.toString().match(re) ||
+        item.up.match(re) ||
+        item.upTo.match(re) ||
+        item.depthBot.match(re) ||
+        item.depthTop.match(re) ||
+/*         item.visibility.match(re) ||*/
+        item.description.match(re) ||
+        item.editor.match(re) ||
         item.date.match(re)
       );
+    },
+
+    /**
+     * Get the style for the row at the specified index.
+     *
+     * @param {number} index The index of the row
+     * @returns {Object} An object containing row style properties
+     */
+    getRowStyle(index) {
+      return {
+        backgroundColor: this.hoveredRow === index ? '#2f3845' : 'transparent',
+        cursor: 'pointer',
+        padding: '8px 16px'
+      };
+    },
+    /**
+     * Update the hoveredRow based on the isHovered flag.
+     *
+     * @param {number} index - The index of the row being hovered.
+     * @param {boolean} isHovered - Indicates if the row is being hovered 
+     *                              (true) or not (false).
+     */
+    setHoveredRow(index, isHovered) {
+      if (isHovered) {
+        this.hoveredRow = index;
+      } else if (this.hoveredRow === index) {
+        this.hoveredRow = -1;
+      }
     },
 
   }
@@ -306,12 +540,8 @@ export default {
 </script>
 
 <style scoped>
-#wrapper {
-  height: 100%;
-}
-.positionItem {
-  border-top: 2px solid black;
-  border-bottom: 2px solid black;
-  background-color: rgb(221, 221, 221);
+
+.align-right {
+  text-align: right;
 }
 </style>
