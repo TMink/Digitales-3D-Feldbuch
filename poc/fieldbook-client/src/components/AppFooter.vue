8b978c92f8c90de0672747349b7ffe264cd5961a
Meoli <mertens.oliver@gmx.de>
2023-07-28 16:34:21 +0200

    update appFooter - th koeln icon is back

--- poc/fieldbook-client/src/components/AppFooter.vue
+++ poc/fieldbook-client/src/components/AppFooter.vue
@@ -3,8 +3,13 @@
         <v-row>
             <v-col icon class="text-center">
                 {{ new Date().getFullYear() }} — <strong>TH-Köln</strong>
-                <v-btn icon class="ma-1" variant="text" href="https://www.th-koeln.de/informatik-und-ingenieurwissenschaften/fakultaet-fuer-informatik-und-ingenieurwissenschaften_2466.php" target="_blank">
-                    <v-img src="/THKöln_Icon.png"></v-img>
+                <v-btn  icon 
+                        class="ma-1" 
+                        variant="text" 
+                        href="https://www.th-koeln.de/informatik-und-ingenieurwissenschaften/fakultaet-fuer-informatik-und-ingenieurwissenschaften_2466.php" 
+                        target="_blank"
+                        >
+                    <v-icon><v-img src="/THKöln_Icon.png"></v-img></v-icon>
                 </v-btn>
             </v-col>
         </v-row>
