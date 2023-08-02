747ea3ab7ee9453f9a8208a415ae9c37f3d9aab5
JuHardtung <julian.hardtung@smail.fh-koeln.de>
2023-08-02 22:23:37 +0200

    rename drawings to images in PlaceForm

--- poc/fieldbook-client/src/views/PlaceForm.vue
+++ poc/fieldbook-client/src/views/PlaceForm.vue
@@ -6,7 +6,7 @@
           <v-tabs v-model="tab" direction="vertical" color="primary">
             <v-tab value="one" rounded="0"> {{ $t('general') }} </v-tab>
             <v-tab value="two" rounded="0"> {{ $tc('position', 2) }} </v-tab>
-            <v-tab value="three" rounded="0"> {{ $tc('drawing', 2) }} </v-tab>
+            <v-tab value="three" rounded="0"> {{ $tc('image', 2) }} </v-tab>
             <v-tab value="four" rounded="0"> {{ $tc('model', 2) }} </v-tab>
             <v-btn rounded="0" v-on:click="savePlace()" color="primary">
               {{ $t('save') }}
