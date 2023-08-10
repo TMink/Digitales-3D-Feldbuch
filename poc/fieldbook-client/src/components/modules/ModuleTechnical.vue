9782e845cb33ada3745bf203a8b85d3722ba4ca1
Tobias Mink <xmrmagicx@gmail.com>
2023-08-10 10:35:56 +0200

    create new module: technical

---
+++ poc/fieldbook-client/src/components/modules/ModuleTechnical.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-col lg="6" class="pt-0 pl-0 pr-0">
+      <v-card class="pa-4">
+        <v-card-text>
+          <h2 class="text-h6 font-weight-medium pb-2">
+            {{ $t('plane') }}
+          </h2>
+        </v-card-text>
+          <v-textarea 
+              rows="2" 
+              counter 
+              color="primary" 
+              class="ma-1 px-2 pt-2" 
+              v-model="technical"
+              :label="$t('plane')">
+          </v-textarea>
+      </v-card>
+    </v-col>
+  </template>
+  
+  <script>
+  /**
+   * Methods overview:
+   */
+  export default {
+  
+    props: {
+      technicalProp: String,
+    },
+  
+    emits: ['dataToModelViewer'],
+  
+    /**
+     * Reactive Vue.js data
+     */
+    data() {
+      return {
+        plane: null,
+        pathNames: null,
+      }
+    },
+  
+    watch: {
+      'technical': {
+        handler: function() {
+          if (this.technical != null) {
+            this.$emit("dataToModelViewer", [ 'technical', this.technical ]);
+          }
+        }
+      },
+    },
+  
+    created() {
+      this.technical = this.technical;
+    },
+  
+  };
+  
+  </script>
+  
+  <style scoped></style>
