943fbf7d6d6e27fb0b9cb8693ad3b734f95f108a
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 18:13:42 +0100

    create function updateObjects
    Part of UpdateIndexedDB class

--- src/frontend/src/components/3dFunctions/UpdateIndexedDB.js
+++ src/frontend/src/components/3dFunctions/UpdateIndexedDB.js
@@ -39,4 +39,48 @@ export class UpdateIndexedDB {
 
     fromOfflineDB.updateObject( newCamera, 'Cameras', 'cameras' );
   }
+
+  async updateObjects( placeModelsInScene, positionModelsInScene, sceneMain ) {
+
+    /* Update place models */
+    for ( var i = 0; i < placeModelsInScene.length; i++ ) {
+      const modelID = placeModelsInScene[ i ]._id;
+      this.updateObjectOpacityAndColor(modelID, 'places', sceneMain);
+    }
+
+    /* Update position models */
+    for ( var u = 0; u < positionModelsInScene.length; u++ ) {
+      const modelID = positionModelsInScene[ u ]._id;
+      const modelInScene = sceneMain.getObjectByName( modelID );
+      const modelInDB = await fromOfflineDB.getObject( modelID, 'Models',
+        'positions' );
+
+      const utilities = new Utilities()
+      var groupObject = utilities.getGroup( modelInScene );
+
+      /* Get local Rotion */
+      const euler = groupObject.rotation
+      const rotation = [ euler._x, euler._y, euler._z, euler._order ]
+      
+      /* Get world scale */
+      const worldScale = new THREE.Vector3();
+      groupObject.getWorldScale( worldScale );
+      const scale = [ worldScale.x, worldScale.y, worldScale.z ];
+
+      /* Get world position */
+      const worldPosition = new THREE.Vector3();
+      groupObject.getWorldPosition( worldPosition );
+      const coordinates = [ worldPosition.x, worldPosition.y,
+        worldPosition.z ];
+
+      modelInDB.color = "#" + modelInScene.material.color.getHexString();
+      modelInDB.opacity = modelInScene.material.opacity;
+      modelInDB.coordinates = coordinates;
+      modelInDB.scale = scale;
+      modelInDB.rotation = rotation;
+
+      await fromOfflineDB.updateObject( modelInDB, 'Models', 'positions' );
+    }
+
+  }
 }
