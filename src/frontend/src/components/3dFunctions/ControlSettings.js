eea9891cc91d10812327fba779595839049c306d
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:29:54 +0100

    create new class file ControlSettings
    Part of the outsourcing process; Will include every function to modify the controls in scene

---
+++ src/frontend/src/components/3dFunctions/ControlSettings.js
@@ -0,0 +1,14 @@
+/*
+ * Created Date: 15.01.2024 12:38:45
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 15.01.2024 13:48:52
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+import { Utilities } from './Utilities.js';
+
+export class ControlSettings {
+}
