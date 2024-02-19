6fc8b7b7a6b105942e3ba031244f26eabdca590f
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 18:12:46 +0100

    create function updateCamera
    Part of UpdateIndexedDB class

--- src/frontend/src/components/3dFunctions/UpdateIndexedDB.js
+++ src/frontend/src/components/3dFunctions/UpdateIndexedDB.js
@@ -4,4 +4,39 @@ import { Utilities } from './Utilities.js';
 import * as THREE from 'three';
 
 export class UpdateIndexedDB {
+
+  updateCamera( placeID, cameraIDsInDB, cameraInDB, arcballAnchor, cameraMain ) {
+
+    const anchor = []
+    
+    const cameraData = [
+      cameraMain.position.x,
+      cameraMain.position.y,
+      cameraMain.position.z,
+      cameraMain.rotation.x,
+      cameraMain.rotation.y,
+      cameraMain.rotation.z
+    ];
+
+    /* Create new Anchor for arcball controls or   */
+    if ( !cameraIDsInDB.includes( placeID ) ) {
+      anchor.push( arcballAnchor[ 0 ] );
+      anchor.push( arcballAnchor[ 1 ] );
+      anchor.push( arcballAnchor[ 2 ] );
+    } else {
+      anchor.push( cameraInDB.arcballAnchor[ 0 ] );
+      anchor.push( cameraInDB.arcballAnchor[ 1 ] );
+      anchor.push( cameraInDB.arcballAnchor[ 2 ] );
+    }
+
+    /* Create and save new camera settings  */
+    const newCamera = {
+      _id: placeID,
+      cameraPosition: cameraData,
+      arcballAnchor: anchor,
+      newObjectsInScene: false
+    };
+
+    fromOfflineDB.updateObject( newCamera, 'Cameras', 'cameras' );
+  }
 }
