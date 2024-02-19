72f6a2de472c1b1b4f2633b1226ce39a8605b357
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:27:13 +0100

    create function createPerspectiveCamera
    Part of CameraSettings class

--- src/frontend/src/components/3dFunctions/CameraSettings.js
+++ src/frontend/src/components/3dFunctions/CameraSettings.js
@@ -15,4 +15,15 @@ import { ControlSettings } from './ControlSettings.js';
 
 
 export class CameraSettings {
+
+  createPerspectiveCamera( canvas ) {
+    const fov = 45;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const near = 0.01;
+    const far = 2000;
+    const perspectiveCamera = new THREE.PerspectiveCamera( fov, aspect, near, 
+      far );
+    
+    return perspectiveCamera;
+  }
 }
