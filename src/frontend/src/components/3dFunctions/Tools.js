4c4aebeebacc4aa3bc46994169144fe5240a80da
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 18:08:23 +0100

    create function saveLineTitle
    Part of LineTool class

--- src/frontend/src/components/3dFunctions/Tools.js
+++ src/frontend/src/components/3dFunctions/Tools.js
@@ -88,6 +88,56 @@ export class LineTool {
       measureTool.texttoken = false;
     }
   }
+
+  async saveLineTitle( root, t, measureTool, scene ) {
+    let idToBeRenamed = null;
+    let token = true;
+
+    const linesInDB = await fromOfflineDB.getAllObjects(
+      'Lines', 'lines' );
+    
+    /* New names cant be blank, undefined or already taken */
+    /* Check if new name is " " or undefined */
+    if( measureTool.textField == "" || measureTool.textField == undefined ) {
+      token = false
+      root.vtoast.show({ message: t('Please enter a name first')});
+    } else {
+      /* Check if the name is already taken */
+      var notTaken = true
+      linesInDB.forEach( line => {
+        if( line.name.split(" -")[0] === measureTool.textField ) {
+          notTaken = false;
+        }
+      } )
+      if( !notTaken ) {
+        token = false;
+        root.vtoast.show({ message: t('Name already taken')});
+      }
+    }
+
+    /* If the new name meet the criteria from above */
+    if( token ) {
+      measureTool.infoBlock.forEach( element => {
+        if ( element.name === measureTool.title ) {
+          element.name = measureTool.textField + " - " + measureTool.title.split("- ")[1];
+          idToBeRenamed = element._id;
+        }
+      } )
+  
+      /* Rename line in IndexedDb */
+      const lineInDB = linesInDB.find( e => e._id === idToBeRenamed );
+      lineInDB.name = measureTool.textField + " - " + lineInDB.name.split('- ')[1];
+      lineInDB.lable.distance = measureTool.textField + " \n " + lineInDB.name.split('- ')[1];
+      await fromOfflineDB.updateObject( lineInDB, 'Lines', 'lines' );
+
+      measureTool.textField = measureTool.textField + " - " + lineInDB.name.split('- ')[1];
+  
+      this.updateLineMenue( measureTool );
+      this.updateLineInnerText( lineInDB.line.name, 
+        measureTool.textField.split('- ')[0] + " \n " + lineInDB.name.split('- ')[1], scene );
+      measureTool.texttoken = true;
+    }
+  }
 }
 export class ModelInteraktion {
 }
