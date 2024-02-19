84a0b3cd7726c03f4e093e6e7ef5ab431a577d1d
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:47:47 +0100

    create function mainInit
    Part of Initialisations class

--- src/frontend/src/components/3dFunctions/Initialisations.js
+++ src/frontend/src/components/3dFunctions/Initialisations.js
@@ -55,4 +55,80 @@ export class Initialisations {
     // Variables
     params.lineID = String(Date.now());
   }
+
+  mainInit( params ) {
+    /* All loaded meshes in main scene */
+    params.objectsInMain = [];
+
+    /* Renderer */
+    params.renderer = new THREE.WebGLRenderer( {
+      canvas: params.canvas,
+      antialias: true
+    } );
+    params.renderer.setSize( params.canvas.clientWidth,
+      params.canvas.clientHeight );
+    params.renderer.setClearColor( params.sceneParams.bgColor, 1 );
+    params.renderer.shadowMap.enabled = true;
+    params.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    params.renderer.outputEncoding = THREE.SRGBColorSpace;
+      
+    // Scene
+    params.scene = new THREE.Scene();
+    params.scene.name = "SceneMain"
+
+    // Camera
+    params.camera = this.cameraSettings.createPerspectiveCamera( 
+      params.canvas );
+    params.camera.name = "CameraMain"
+
+    // Light
+    params.light = new THREE.AmbientLight( params.sceneParams.lightColor );
+    params.scene.add( params.light );
+    params.light.name = "LightMain"
+
+    // Controls
+    params.arcBallControls = new ArcballControls( params.camera,
+      params.renderer.domElement, params.scene );
+    params.arcBallControls._maxDownTime = 0;
+    params.arcBallControls._maxInterval = 0;
+    params.arcBallControls.cursorZoom = true;
+    params.arcBallControls.setGizmosVisible( false );
+
+    params.transformControls = new TransformControls( params.camera,
+      params.renderer.domElement );
+    params.transformControls.addEventListener( 'dragging-changed', e => {
+      params.arcBallControls.enabled = !e.value;
+    } );
+
+    params.scene.add( params.transformControls );
+
+    // Raycaster outlines
+    params.raycaster = new THREE.Raycaster();
+    params.pointer = new THREE.Vector2();
+    
+    // Post processing
+    params.composer = new EffectComposer( params.renderer );
+
+    params.renderPass = new RenderPass( params.scene, params.camera );
+    params.composer.addPass( params.renderPass );
+
+    params.outlinePass = new OutlinePass( new THREE.Vector2(
+      params.canvas.clientWidth, params.canvas.clientHeight ),
+      params.scene, params.camera );
+    params.outlinePass.hiddenEdgeColor.set( params.sceneParams.outline.color )
+    params.outlinePass.pulsePeriod = params.sceneParams.outline.pulsePeriod;
+    params.outlinePass.edgeStrength = params.sceneParams.outline.edgeStrength;
+    params.outlinePass.edgeThickness = params.sceneParams.outline.edgeThickness;
+    params.outlinePass.edgeGlow = params.sceneParams.outline.edgeGlow;
+    params.composer.addPass( params.outlinePass );
+
+    // Shader
+    params.effectFXAA = new ShaderPass( FXAAShader );
+    params.effectFXAA.uniforms[ 'resolution' ].value.set(
+      1 / params.canvas.clientWidth, 1 / params.canvas.clientHeight );
+    params.composer.addPass( params.effectFXAA );
+
+    params.gammaCorrectionShader = new ShaderPass( GammaCorrectionShader );
+    params.composer.addPass( params.gammaCorrectionShader )
+  }
 }
