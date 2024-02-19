c631a7f63c8aea47034035ad9873130195451f98
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 18:04:31 +0100

    create file tools
    Part of the outsourcing process; Will include every class of every tool

--- src/frontend/src/components/3dFunctions/Tools.js
+++ src/frontend/src/components/3dFunctions/Tools.js
@@ -1,2 +1,18 @@
+/*
+ * Created Date: 15.01.2024 11:05:52
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 19.02.2024 17:15:51
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+import * as THREE from 'three';
+import { CSS2DObject } from 
+  'three/examples/jsm/renderers/CSS2DRenderer'
+import { fromOfflineDB } from '../../ConnectionToOfflineDB.js'
+import { toRaw } from 'vue';
+
 export class LineTool {
 }
