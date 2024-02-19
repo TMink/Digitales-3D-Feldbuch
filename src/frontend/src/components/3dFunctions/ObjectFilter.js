523a833b298b37986e2f31be883bd1a1341415a1
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:52:08 +0100

    create function fieldSearch
    Part of ObjectFilter class

--- src/frontend/src/components/3dFunctions/ObjectFilter.js
+++ src/frontend/src/components/3dFunctions/ObjectFilter.js
@@ -12,4 +12,70 @@ import { UpdateIndexedDB } from './UpdateIndexedDB.js'
 import { fromOfflineDB } from '../../ConnectionToOfflineDB.js'
 
 export class ObjectFilter {
+
+  async fieldSearch( typeSize, chosenObjects, inputs, menues, infos ) {
+    const areNull = [];
+    const notNull = [];
+    var layers = [];
+
+    /* Determin which field is 'null' and which is not */
+    for ( let a = 0; a < typeSize; a++ ) {
+      if ( inputs[ a ] == null ) {
+        areNull.push( a );
+      } else {
+        notNull.push( a );
+      }
+    }
+
+    this.stringReplacer( typeSize, areNull );
+
+    /* No empty fields */
+    if ( notNull.length == typeSize ) {
+      this.choseObjects( typeSize, chosenObjects, inputs, infos )
+    }
+
+    /* Only empty fields */
+    else if ( areNull.length == typeSize ) {
+      this.resetFilter( infos, menues );
+    }
+
+    /* Only one given field */
+    else if ( notNull.length == 1 ) {
+      for ( let b = 0; b < infos.length; b++ ) {
+        this.fillAll( menues[ notNull[ 0 ] ], infos[ b ][ notNull[ 0 ] ] );
+        layers.push( [infos[ b ][ notNull[ 0 ] ], inputs[ notNull[ 0 ] ]] );
+        for( let c = 0; c < areNull.length; c++ ) {
+          this.layering( layers, menues[ areNull[ c ] ], 
+          infos[ b ][ areNull[ c ] ]);
+        }
+        layers = [];
+      }
+    }
+
+    /* Unkown number of given and empty fields */
+    else if ( notNull.length > 1 ) {
+      for ( let d = 0; d < infos.length; d++ ) {
+        for ( let e = 0; e < notNull.length; e++ ) {
+          for ( let f = 0; f < notNull.length; f++ ) {
+            if ( f != e ) {
+              layers.push( [ infos[ d ][ notNull[ f ] ], 
+                inputs[ notNull[ f ] ] ] );
+            }
+          }
+          this.layering( layers, menues[ notNull[ e ] ], 
+            infos[ d ][ notNull[ e ] ]);
+          layers = [];
+        }
+        for ( let g = 0; g < areNull.length; g++ ) {
+          for ( let h = 0; h < notNull.length; h++ ) {
+            layers.push( [ infos[ d ][ notNull[ h ] ], 
+                           inputs[ notNull[ h ] ] ] );
+          }
+          this.layering( layers, menues[ areNull[ g ] ], 
+                         infos[ d ][ areNull[ g ] ] );
+          layers = [];
+        }
+      }
+    }
+  }
 }
