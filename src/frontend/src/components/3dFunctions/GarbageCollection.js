7cd4f6fe5a1a70c74343cc461b8e341a99cb7750
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:35:44 +0100

    create new class file GarbageCollection
    Part of the outsourcing process; Will include every function to safely delete cached data for more efficient storage useage

---
+++ src/frontend/src/components/3dFunctions/GarbageCollection.js
@@ -0,0 +1,12 @@
+/*
+ * Created Date: 15.01.2024 17:29:22
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 22.01.2024 16:31:40
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+export class GarbageCollection {
+}
