6a3dad6f4d39d91e241f5905531dd7f9962aa5ba
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:50:11 +0100

    create new class file ObjectFilter
    Part of the outsourcing process; Will include every function of the filter algorithm, for choosing the right object for modifications

---
+++ src/frontend/src/components/3dFunctions/ObjectFilter.js
@@ -0,0 +1,15 @@
+/*
+ * Created Date: 15.01.2024 14:52:11
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 23.01.2024 16:24:34
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+import { UpdateIndexedDB } from './UpdateIndexedDB.js'
+import { fromOfflineDB } from '../../ConnectionToOfflineDB.js'
+
+export class ObjectFilter {
+}
