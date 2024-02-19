664cc0cd6fc126fb843b7e7416784e23af4d6e71
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 18:17:44 +0100

    create new class file Utilities
    Part of the outsourcing process; Will include utility functions, which will be used all over the code

---
+++ src/frontend/src/components/3dFunctions/Utilities.js
@@ -0,0 +1,16 @@
+/*
+ * Created Date: 08.01.2024 14:43:50
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 26.01.2024 11:56:59
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+import * as THREE from 'three';
+import { fromOfflineDB } from '../../ConnectionToOfflineDB.js'
+import { toRaw } from 'vue';
+
+export class Utilities {
+}
