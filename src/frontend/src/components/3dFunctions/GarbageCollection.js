79ed23aa7295cc688f4273d473ac333b5d5a8b38
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:36:56 +0100

    create function clearCanvas
    Part of GarbageCollection class

--- src/frontend/src/components/3dFunctions/GarbageCollection.js
+++ src/frontend/src/components/3dFunctions/GarbageCollection.js
@@ -9,4 +9,19 @@
  */
 
 export class GarbageCollection {
+  
+  async clearCanvas( scene, data ) {
+
+    if( scene.name == 'SceneMain' ) {
+      this.removeObjects( scene, data.objects );
+      this.removeLines( scene, data.infoBlock );
+      this.removeRenderer( data.renderer );
+
+      data.controls.detach()  
+    } else if( scene.name == 'SceneSub' ) {
+      this.removeObjects( scene, data.objects );
+      this.removeRenderer( data.renderer );
+    }
+    
+  }
 }
