d9f76fbc618922a0efb1b8d1e94f78244f982bfa
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:30:49 +0100

    create function attachTransformControls
    Part of ControlSettings class

--- src/frontend/src/components/3dFunctions/ControlSettings.js
+++ src/frontend/src/components/3dFunctions/ControlSettings.js
@@ -11,4 +11,17 @@
 import { Utilities } from './Utilities.js';
 
 export class ControlSettings {
+  
+  attachTransformControls( posMods, posObject, tControls ) {
+    if ( !posMods.attachTransformControls ) {
+      posMods.attachTransformControls = true;
+      if ( posObject.chosenfinalModelGroup ) {
+        tControls.detach();
+        tControls.attach(posObject.chosenfinalModelGroup);
+      }
+    } else {
+      posMods.attachTransformControls = false;
+      tControls.detach();
+    }
+  }
 }
