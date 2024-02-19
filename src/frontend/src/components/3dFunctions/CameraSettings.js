a0ab920fe7a0a72a3fe8676c2bf613dca95c6c90
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:24:11 +0100

    create new class file CameraSettings
    Part of the outsourcing process; Will include every function to modify the camera in scene

---
+++ src/frontend/src/components/3dFunctions/CameraSettings.js
@@ -0,0 +1,18 @@
+/*
+ * Created Date: 15.01.2024 13:52:27
+ * Author: Tobias Mink
+ * 
+ * Last Modified: 24.01.2024 16:58:10
+ * Modified By: Tobias Mink
+ * 
+ * Description: 
+ */
+
+import * as THREE from 'three';
+
+import { Utilities } from './Utilities.js';
+import { ControlSettings } from './ControlSettings.js';
+
+
+export class CameraSettings {
+}
