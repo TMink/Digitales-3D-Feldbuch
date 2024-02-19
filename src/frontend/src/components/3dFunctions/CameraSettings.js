bb4fccbcc19269c6e8fa319ec7b8dd6cad9dd24c
Tobias Mink <xmrmagicx@gmail.com>
2024-02-19 17:28:39 +0100

    create function updateCamera2
    Part of CameraSettings class

--- src/frontend/src/components/3dFunctions/CameraSettings.js
+++ src/frontend/src/components/3dFunctions/CameraSettings.js
@@ -67,4 +67,49 @@ export class CameraSettings {
       controls.update();
     }
   }
+  
+  async updateCamera2( objectName, placeID, camera, cameraIDsInDB, cameraInDB,
+    cameraData, controls, scene, arcballAnchor ) {
+    
+    if ( cameraIDsInDB.includes( placeID ) ) {
+      const utilities = new Utilities()
+      const controlSettings = new ControlSettings()
+
+      /* Update current camera state with camera data from IndexedDB */
+      const cameraPosition = cameraInDB.cameraPosition;
+      cameraData.position = [ cameraPosition[ 0 ], cameraPosition[ 1 ],
+      cameraPosition[ 2 ] ];
+      cameraData.rotation = [ cameraPosition[ 3 ], cameraPosition[ 4 ],
+      cameraPosition[ 5 ] ];
+
+      /* Restore saved anchor position for the arcball target */
+      controlSettings.restoreArcballAnchor( cameraInDB.arcballAnchor, 
+        controls );
+      
+      /* Set new camera state */
+      utilities.setCamera( camera, cameraData );
+    } else {
+      const utilities = new Utilities()
+      
+      /* Get center of place model as Vec3 */
+      const object = scene.getObjectByName( objectName );
+      const center = utilities.getModelCenter( object );
+
+      /* Define an anchor point for the arcball */
+      arcballAnchor.push( center.x );
+      arcballAnchor.push( center.y );
+      arcballAnchor.push( center.z );
+
+      /* Set center as the new arcball target */
+      controls.target.set( center.x, center.y, center.z );
+
+      /* Move camera to the object */
+      camera.position.set( center.x, center.y - 15, center.z );
+      cameraData = utilities.getCameraData( camera );
+
+      /* Save changes made to the arcball control settings */
+      controls.update();
+    }
+  }
+
 }
