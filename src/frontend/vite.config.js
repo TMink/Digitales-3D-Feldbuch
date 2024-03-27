dfc853ffd4cd3f3895d8dfe453dad10d7ce8f0d2
Julian Hardtung <julian.hardtung@smail.fh-koeln.de>
2024-03-27 20:35:06 +0100

    add default vite packaging path

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: true,
