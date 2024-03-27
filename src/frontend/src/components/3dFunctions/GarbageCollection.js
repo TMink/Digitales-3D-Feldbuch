0009cb07e4defdacaab4371fcf290059226e549d
Tobias Mink <xmrmagicx@gmail.com>
2024-03-27 20:03:44 +0100

    add annotation tool to garbage collection

--- src/frontend/src/components/3dFunctions/GarbageCollection.js
+++ src/frontend/src/components/3dFunctions/GarbageCollection.js
@@ -2,7 +2,7 @@
  * Created Date: 15.01.2024 17:29:22
  * Author: Tobias Mink
  * 
- * Last Modified: 21.02.2024 23:58:43
+ * Last Modified: 27.03.2024 18:38:22
  * Modified By: Tobias Mink
  * 
  * Description: 
@@ -10,17 +10,17 @@
 
 export class GarbageCollection {
   
-  async clearCanvases( main, sub, infoBlock ) {
+  async clearCanvases( main, sub, mmToolInfoBlock, anToolInfoBlock ) {
 
       this.removeObjects( main.scene, main.objects );
-      this.removeLines( main.scene, infoBlock );
+      this.removeLines( main.scene, mmToolInfoBlock );
+      this.removeAnnotations( main.scene, anToolInfoBlock );
       this.removeRenderer( main.renderer );
       main.arcBallControls.dispose()
       main.transformControls.detach()
 
       this.removeObjects( sub.scene, sub.object );
       this.removeRenderer( sub.renderer );
-    
     
   }
 
@@ -50,6 +50,27 @@ export class GarbageCollection {
           ball.geometry.dispose();
           scene.remove( ball );
         } )
+      } )
+    }
+  }
+
+  removeAnnotations( scene, infoBlock ) {
+    if( infoBlock.length > 0 ) {
+      infoBlock.forEach( elem => {
+        console.log("Garbage collection")
+        console.log(elem)
+        const lableInScene = scene.getObjectByName( elem._id )
+        const boxInScene = scene.getObjectByName( elem.boxName )
+        
+        /* Remove box */
+        boxInScene.remove( lableInScene )
+        boxInScene.material.dispose()
+        boxInScene.geometry.dispose()
+        scene.remove( boxInScene )
+        
+        /* Remove lable */
+        lableInScene.remove()        
+
       } )
     }
   }
