b31597adbb1aa452834d62296b72d50043922c61
Tobias Mink <xmrmagicx@gmail.com>
2024-04-18 18:30:01 +0200

    update garbage collection

--- src/frontend/src/components/3dFunctions/GarbageCollection.js
+++ src/frontend/src/components/3dFunctions/GarbageCollection.js
@@ -2,7 +2,7 @@
  * Created Date: 15.01.2024 17:29:22
  * Author: Tobias Mink
  * 
- * Last Modified: 04.04.2024 22:22:07
+ * Last Modified: 18.04.2024 15:15:14
  * Modified By: Tobias Mink
  * 
  * Description: The Process that will happen, wenn ever the 3DView is closed and
@@ -22,19 +22,33 @@ export class GarbageCollection {
    */
   async clearCanvases( main, sub ) {
     /* Dispose scenes */
-    this.disposeOfObjects( main );
-    main.arcBallControls.dispose();
-    main.transformControls.dispose();
-    this.disposeOfObjects( sub );
-    sub.orbitControls.dispose();
+    this.disposeOfObjects( main.components.scene[0].data );
+    for( const control in main.components.contorls ) {
+      control.data.dispose();
+    }
+    if( sub.components.scene[0].data != null ) {
+      this.disposeOfObjects( sub.components.scene[0].data );
+      sub.orbitControls.dispose();
+    }
+  }
+
+  async cleanEnv( env ) {
+    if( env.components.scene[0].data != null ) {
+      this.disposeOfObjects( env.components.scene[0].data )
+    }
+    for( const [_, controls] of Object.entries(env.components.controls) ) {
+      if( controls.data != null ) {
+        controls.data.dispose()
+      }
+    }
   }
 
   /**
    * Disposes all objects of given scene.
    * @param { object } whichScene 
    */
-  async disposeOfObjects( whichScene ) {
-    whichScene.scene.traverse( o => {
+  async disposeOfObjects( scene, renderer ) {
+    scene.traverse( o => {
       if (o.geometry) {
         o.geometry.dispose()                       
       }
@@ -51,18 +65,19 @@ export class GarbageCollection {
       }
     } )
 
-    while( whichScene.scene.children.length > 0 ) {
-      await Promise.all( whichScene.scene.children.map( async ( child ) => {
+    while( scene.children.length > 0 ) {
+      await Promise.all( scene.children.map( async ( child ) => {
         if( child.children.length ) {
           if( child.children[0].type == "Object3D" ) {
             child.remove( child.children[0] )
           }
         }
-        whichScene.scene.remove( child )
+        scene.remove( child )
       } ) )
     }
-    whichScene.renderer && whichScene.renderer.renderLists.dispose()
-    whichScene.renderer = null
+    renderer && renderer.renderLists.dispose()
+    renderer = null
+  }
   }
   
 }
