a1fd63ab3f327b2586972a05791d4021fb8ac747
Tobias Mink <xmrmagicx@gmail.com>
2024-09-24 13:39:10 +0200

    Update StratiTool.vue
    Add basic layout and resize function; Introduce <script setup> alongside <script>

--- src/frontend/src/views/StratiTool.vue
+++ src/frontend/src/views/StratiTool.vue
@@ -10,21 +10,96 @@
 
 <template>
   <div id="wrapper">
-    <Navigation active_tab_prop="1" />
-
+    <Navigation active_tab_prop="2" />
+      
+      <v-row no-gutters>
+        
+        <!-- Left -->
+        <v-col cols="2">
+          <v-row no-gutters style="height:fit-content">
+            <v-card class="border-lg d-flex align-center justify-center" height="100px" width="100%">
+              Glossar
+            </v-card>  
+          </v-row>
+          <v-row no-gutters style="height:fit-content">
+            <!-- <div class="border-lg d-flex justify-end"></div> -->
+            <v-card class="border-lg d-flex align-center justify-center" :height="windowHeight - 212" width="100%">
+              Module
+            </v-card>
+          </v-row>
+        </v-col>
+        
+        <!-- Middle -->
+        <v-col cols="8">
+          <v-row no-gutters style="height:fit-content">
+            <v-card class="border-lg d-flex align-center justify-center" height="100px" width="100%">
+              Harris-Matrizen
+            </v-card>   
+          </v-row>
+          <v-row no-gutters style="height:fit-content">
+            <v-card class="border-lg d-flex align-center justify-center" height="100px" width="100%">
+              Units
+            </v-card>
+          </v-row>
+          <v-row no-gutters style="height:fit-content;">
+            <canvas id="canvas" class="border-lg" :height="windowHeight - 320" style="position:absolute; z-index: 1"></canvas>
+            <v-card id="graph" class="border-lg d-flex align-center justify-center" :height="windowHeight - 312" width="100%" style="position:relative; z-index: 0; opacity: 1;">
+              <VueFlow :nodes="nodes" :edges="edges">
+                <Background />
+              </VueFlow>
+            </v-card>
+          </v-row>
+        </v-col>
+        
+        <!-- Right -->
+        <v-col cols="2">
+          <v-row no-gutters style="height:fit-content">
+            <v-card class="border-lg d-flex align-center justify-center" height="100px" width="100%">
+              Hilfe
+            </v-card>   
+          </v-row>
+          <v-row no-gutters style="height:fit-content">
+            <v-card class="border-lg d-flex align-center justify-center" height="100px" width="100%">
+              Modus wechseln
+            </v-card>
+          </v-row>
+          <v-row no-gutters style="height:fit-content">
+            <v-card id="infobereich" class="border-lg d-flex align-center justify-center" :height="windowHeight - 312" width="100%">
+              Infobereich
+            </v-card>
+          </v-row>
+        </v-col>
+      </v-row>
+      
   </div>
 </template>
 
-<script>
-import Navigation from '../components/Navigation.vue';
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  import Navigation from '../components/Navigation.vue';
+
+  // get data for resizing
+  const { width, height } = useWindowSize();
+  const windowHeight = height;
+  const windowWidth = width;
 
 export default{
+  // Everything after mounted
+  onMounted(() => {  
+    // resize canvas
+    window.addEventListener("resize", resizeCanvas);
+    document.getElementById("canvas").setAttribute("width", window.innerWidth - (document.getElementById("infobereich").offsetWidth*2+16))
+    function resizeCanvas() {
+      document.getElementById("canvas").setAttribute("width", window.innerWidth - (document.getElementById("infobereich").offsetWidth*2+16))
+    }
+  })
+</script>
+
+<script>
+export default {
   name: 'StratiTool',
-  components: {
-    Navigation
-  },
-  data() {},
-  mounted() {}
 }
+</script>
 
 </script>
