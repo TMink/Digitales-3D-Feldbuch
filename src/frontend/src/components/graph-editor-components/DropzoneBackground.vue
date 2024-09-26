56d43669df672a86ae182cb13cde55ef4b98f52c
Tobias Mink <xmrmagicx@gmail.com>
2024-09-26 15:17:46 +0200

    Create DropzoneBackground.vue

---
+++ src/frontend/src/components/graph-editor-components/DropzoneBackground.vue
@@ -0,0 +1,22 @@
+<!--
+ Created Date: 24.09.2024 14:49:53
+ Author: Tobias Mink
+ 
+ Last Modified: 26.09.2024 15:15:17
+ Modified By: Tobias Mink
+ 
+ Description: 
+ -->
+
+<script lang="ts" setup>
+import { Background } from '@vue-flow/background'
+</script>
+ 
+<template>
+  <div class="dropzone-background">
+    <Background :size="2" :gap="50" color="#42f59e" variant="dots"/>
+    <div class="overlay">
+      <slot />
+    </div>
+  </div>
+</template>
