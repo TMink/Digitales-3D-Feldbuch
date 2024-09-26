91072c8a3b832a128700bce9204a36599b1a45dd
Tobias Mink <xmrmagicx@gmail.com>
2024-09-26 14:48:46 +0200

    Create InterfaceNode.vue

---
+++ src/frontend/src/components/graph-editor-components/CustomNodes/InterfaceNode.vue
@@ -0,0 +1,39 @@
+<!--
+ Created Date: 26.09.2024 12:58:32
+ Author: Tobias Mink
+ 
+ Last Modified: 26.09.2024 14:43:32
+ Modified By: Tobias Mink
+ 
+ Description: 
+ -->
+
+<script setup>
+import { Position, Handle } from '@vue-flow/core'
+  
+// props were passed from the slot using `v-bind="customNodeProps"`
+const props = defineProps(['data', 'type'])
+</script>
+  
+<template>
+  <div class="interface">
+    <Handle id="top_middle" type="target" :position="Position.Top"/>
+    <Handle id="left_middle" type="target" :position="Position.Left"/>
+    <div>{{ data.label }}</div>
+    <Handle id="right_middle" type="target" :position="Position.Right"/>
+    <Handle id="bottom_middle" type="target" :position="Position.Bottom"/>
+  </div>
+</template>
+ 
+<style>
+  .interface {
+    height: 100px;
+    width: 100px;
+    line-height: 100px;
+    text-align: center;
+    color: black;
+    background-color:rgb(104, 65, 15);
+    border: 1px solid #fff;
+    border-radius: 100%;
+  }
+</style>
