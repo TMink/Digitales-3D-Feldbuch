70797e71827f495c2aed5bf6157e515a0d013bb5
Tobias Mink <xmrmagicx@gmail.com>
2024-10-16 05:52:11 +0200

    Update DnD functionality and paramaters of new node (deposit/interface)

--- src/frontend/src/components/graph-editor-components/useDnD.js
+++ src/frontend/src/components/graph-editor-components/useDnD.js
@@ -2,6 +2,7 @@
  * Created Date: 24.09.2024 14:53:58
  * Author: Tobias Mink
  * 
+ * Last Modified: 03.10.2024 15:40:11
  * Modified By: Tobias Mink
  * 
  * Description: 
@@ -10,6 +11,11 @@
 import { useVueFlow } from '@vue-flow/core'
 import { ref, watch } from 'vue'
 
+let count = 0
+
+function getDefaultLabel() {
+  return `Neue Unit_${count++}`
+}
 
 /**
  * @returns {string} - A unique id.
@@ -29,19 +35,25 @@ const state = {
   draggedType: ref(null),
   isDragOver: ref(false),
   isDragging: ref(false),
+  nodeDropped: ref(false),
 }
 
 export default function useDragAndDrop() {
+  const { draggedType, isDragOver, isDragging, nodeDropped } = state;
 
+  const { addNodes, screenToFlowCoordinate, onNodesInitialized, updateNode, onNodeMouseEnter } = useVueFlow();
 
   watch(isDragging, (dragging) => {
     document.body.style.userSelect = dragging ? 'none' : ''
   })
 
+  function onDragStart(event, type, highestDefaultCount) {
     if (event.dataTransfer) {
       event.dataTransfer.setData('application/vueflow', type)
       event.dataTransfer.effectAllowed = 'move'
     }
+
+    count = highestDefaultCount++
 
     draggedType.value = type
     isDragging.value = true
@@ -74,6 +86,7 @@ export default function useDragAndDrop() {
     isDragging.value = false
     isDragOver.value = false
     draggedType.value = null
+    nodeDropped.value = true
     document.removeEventListener('drop', onDragEnd)
   }
 
@@ -94,6 +107,58 @@ export default function useDragAndDrop() {
       id: nodeId,
       type: draggedType.value,
       position,
+      data: null
+    }
+
+    switch( draggedType.value ) {
+      case "deposit" :
+        newNode.data = { 
+          label: getDefaultLabel(), 
+          description: null, 
+          relations: [], 
+          groupIDs: null, 
+          volumeID: null, 
+          findingID: null,
+          nodeStyle: "notClicked_deposit",
+          selected: false,
+          styles: { 
+            top_middle: "height: 8px; width: 18px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%;",
+            bottom_middle: "height: 8px; width: 18px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%;",
+            left_middle: "height: 18px; width: 8px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%;",
+            right_middle: "height: 18px; width: 8px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%;"
+          }, 
+          validConnections: { 
+            top_middle: true, 
+            bottom_middle: true, 
+            left_middle: true, 
+            right_middle: true 
+          }
+        };
+        break;
+      case "interface":
+        newNode.data = { 
+          label: getDefaultLabel(), 
+          description: null, 
+          relations: [], 
+          groupIDs: null, 
+          surfaceID: null,
+          nodeStyle: "notClicked_interface",
+          selected: false,
+          styles: {
+            top_middle: "height: 8px; width: 18px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%; border-top-left-radius: 100%; border-top-right-radius: 100%;",
+            bottom_middle: "height: 8px; width: 18px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%; border-bottom-left-radius: 100%; border-bottom-right-radius: 100%;",
+            left_middle: "height: 18px; width: 8px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%; border-bottom-left-radius: 100%; border-top-left-radius: 100%;",
+            right_middle: "height: 18px; width: 8px; border: 2px; border-style: solid; border-color: #FFFFFF; border-radius: 0%; border-bottom-right-radius: 100%; border-top-right-radius: 100%;"
+          }, 
+          validConnections: { 
+            top_middle: true, 
+            bottom_middle: true, 
+            left_middle: true, 
+            right_middle: true 
+          }
+        };
+        break;
+      default: 
     }
 
     /**
@@ -116,6 +181,7 @@ export default function useDragAndDrop() {
     draggedType,
     isDragOver,
     isDragging,
+    nodeDropped,
     onDragStart,
     onDragLeave,
     onDragOver,
